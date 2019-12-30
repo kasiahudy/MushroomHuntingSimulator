9b4cc7ff0b76f47808ce0f7a1c411d0961d09fc6
Kasia <k.hudy96@gmail.com>
2019-12-30 13:40:09 +0100

    Added spawning mushrooms

---
+++ MushroomHuntingSimulator/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField]
+    Mushroom[] _mushrooms;
+
+    [SerializeField]
+    Transform _root;
+
+    float circleRadius = 250f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnMushrooms();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnMushrooms()
+    {
+
+        for (int i = 0; i < 100; i++)
+        {
+            foreach (Mushroom mushroom in _mushrooms)
+            {
+                if (Random.value < mushroom.spawnProbability)
+                {
+                    Vector3 rndPos = Random.insideUnitSphere * circleRadius + _root.position;
+                    rndPos.y = 0.227f;
+
+                    Mushroom prefab = mushroom;
+                    Instantiate<Mushroom>(prefab, rndPos, Quaternion.identity, _root);
+                }
+            }
+        }
+            
+
+
+    }
+}
