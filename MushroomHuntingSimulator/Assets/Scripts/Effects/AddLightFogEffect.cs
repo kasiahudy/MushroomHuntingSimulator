30bed8def34b42a5188c7f97220051caddb04580
Kasia <k.hudy96@gmail.com>
2020-01-17 14:43:25 +0100

    Added morel special effect

---
+++ MushroomHuntingSimulator/Assets/Scripts/Effects/AddLightFogEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddLightFogEffect : Effect
+{
+    private string effectInfo = "Health points reduce faster for 10s!";
+
+    public override void Activate()
+    {
+        RenderSettings.fog = true;
+        mushroomCollectInfo.setEffectInfo(effectInfo, IsNegative());
+    }
+
+    public override void Deactivate()
+    {
+        RenderSettings.fog = false;
+    }
+
+    public override bool IsNegative()
+    {
+        return true;
+    }
+}
